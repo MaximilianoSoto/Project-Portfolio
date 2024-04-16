38f79d384706c0a155c3d71220408ee817e0b1e2
MaximilianoSoto <maximiliano.soto.guardia99@gmail.com>
2024-04-15 20:39:22 -0400

    update v5

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,7 +17,7 @@ import { SVG } from "../assets/SVG";
 
 export const Projects = () => {
   return (
-    <section id="Projects" className=" min-h-[85vh] md:min-h-[78vh]   ">
+    <section id="Projects" className=" min-h-[85vh] md:min-h-[90vh]">
       <div className="w-[90%] p-2  pt-[20%]  md:p-0  h-screen md:w-[65%] md:pt-[5%] mx-auto  ">
         <motion.div
           variants={fadeIn("down", 0.3)}
@@ -50,50 +50,15 @@ export const Projects = () => {
               {ProjectsList &&
                 ProjectsList.map((item) => (
                   <SwiperSlide key={item.titulo}>
-                    <article className="mb-12  md:mb-16 flex  flex-col mx-auto   bg-purple-900/70 border-4 rounded-lg border-accent/60 p-3">
-                      <header className="h-[20vh]">
-                        <img
-                          className="w-full h-[20vh] rounded-lg"
-                          alt="preview-image"
-                          src={item.preview}
-                        />
+                    <article className="mb-20 border-2 h-[500px]  md:h-[700px] flex flex-col items-center p-2 ">
+                      <header className="border-2 w-full text-center h-[200px] md:h-[250px]">
+                        imagen
                       </header>
-                      <main className="max-h-[200px] ">
-                        <h1 className="text-[20px] md:text-[25px]  p-2 mt-3 font-primary leading-8 font-semibold text-accent  ">
-                          {item.titulo}
-                        </h1>
-                        <p className=" leading-[1.2rem] lg:leading-[2rem]  p-2 text-[17px]  dm:text-[20px] mt-2 font-secondary text-white">
-                          {item.descripcion}
-                        </p>
+                      <main className="border-2 w-full text-center h-[200px]   md:h-[250px]">
+                        titulo y descripcion
                       </main>
-                      <footer className="mt-2  md:mt-5">
-                        <div className=" flex gap-2 flex-wrap">
-                          {item.tecnologias.map((item) => (
-                            <TecnologiesComponent key={item} item={item} />
-                          ))}
-                        </div>
-                        <div className="mt-10  md:mt-4 mx-auto">
-                          <div className="flex gap-1  md:gap-5 ">
-                            <Button className="flex items-center btn  ">
-                              <a
-                                target="_blank"
-                                className="font-secondary   md:text-xl "
-                              >
-                                Ver Proyecto
-                              </a>
-                              <SVG />
-                            </Button>
-                            <Button className="flex items-center  gap-x-1 btn">
-                              <FaGithub className="w-6 h-4" />
-                              <a
-                                target="_blank"
-                                className="font-secondary mt-1  md:text-xl "
-                              >
-                                GitHub
-                              </a>
-                            </Button>
-                          </div>
-                        </div>
+                      <footer className="border-2 w-full text-center h-[100px] md:h-[200px]">
+                        habilidades y botones
                       </footer>
                     </article>
                   </SwiperSlide>
--- src/saves/cardComponente.jsx
+++ src/saves/cardComponente.jsx
@@ -1,52 +1,86 @@
-<Card className="max-w-[500px] h-[70vh] bg-orange-300 shadow-lg m-3 bg-purple-900/70 border-2 border-accent/60   ">
-  <CardHeader className="" floated={false} color="blue-gray">
-    <img className="" src={preview} />
-  </CardHeader>
-  <CardBody>
-    <div className=" ">
-      <Typography
-        variant="h5"
-        className="font-primary  font-semibold text-accent  "
-      >
-        {titulo}
-      </Typography>
-    </div>
-    <Typography className="font-secondary text-white    ">
-      {descripcion}
-    </Typography>
-    <div className="inline-flex flex-wrap items-center gap-3 ">
-      {tecnologias &&
-        tecnologias.map((item, index) => (
-          <TecnologiesComponent key={index} item={item} />
-        ))}
-    </div>
-  </CardBody>
-  <CardFooter className="pt-3 flex gap-x-3">
-    <Button className="flex items-center btn ">
-      <a href={demo} target="_blank" className="font-secondary ">
-        Ver Proyecto
-      </a>
-      <svg
-        aria-hidden="true"
-        className="ml-2 mb-1 w-4 h-4"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 
-                      01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-    </Button>
-
-    <Button className="flex items-center gap-x-2 btn">
-      <FaGithub className="w-6 h-4" />
-      <a href={github} target="_blank" className="font-secondary mt-1 ">
-        GitHub
-      </a>
-    </Button>
-  </CardFooter>
-</Card>;
+<section id="Projects" className=" min-h-[85vh] md:min-h-[78vh]   ">
+  <div className="w-[90%] p-2  pt-[20%]  md:p-0  h-screen md:w-[65%] md:pt-[5%] mx-auto  ">
+    <motion.div
+      variants={fadeIn("down", 0.3)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.3 }}
+    >
+      <h1 className=" md:text-[35px] font-semibold leading-[0.8]  font-primary  text-white text-center">
+        MIS PROYECTOS
+      </h1>
+      <div className="pt-5">
+        <Swiper
+          breakpoints={{
+            340: {
+              slidesPerView: 1,
+              spaceBetween: 15,
+            },
+            700: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="max-w-[100%] "
+        >
+          {ProjectsList &&
+            ProjectsList.map((item) => (
+              <SwiperSlide key={item.titulo}>
+                <article className="md:h-[700px]  mb-12  md:mb-16 flex  flex-col mx-auto   bg-purple-900/70 border-4 rounded-lg border-accent/60 p-3">
+                  <header className="h-[20vh]">
+                    <img
+                      className="w-full h-[20vh] rounded-lg"
+                      alt="preview-image"
+                      src={item.preview}
+                    />
+                  </header>
+                  <main className="max-h-[200px] ">
+                    <h1 className="text-[20px] md:text-[25px]  p-2 mt-2 font-primary leading-8 font-semibold text-accent  ">
+                      {item.titulo}
+                    </h1>
+                    <p className=" leading-[1.2rem] lg:leading-[1.5rem]  p-2 text-[17px]  dm:text-[20px] mt-2 font-secondary text-white">
+                      {item.descripcion}
+                    </p>
+                  </main>
+                  <footer className="mt-2  md:mt-5 bg-orange-600 flex flex-col h-full  justify-end">
+                    <div className=" flex gap-2 flex-wrap">
+                      {item.tecnologias.map((item) => (
+                        <TecnologiesComponent key={item} item={item} />
+                      ))}
+                    </div>
+                    <div className="mt-10  md:mt-4 mx-auto">
+                      <div className="flex gap-1  md:gap-5 ">
+                        <Button className="flex items-center btn  ">
+                          <a
+                            target="_blank"
+                            className="font-secondary   md:text-xl "
+                          >
+                            Ver Proyecto
+                          </a>
+                          <SVG />
+                        </Button>
+                        <Button className="flex items-center  gap-x-1 btn">
+                          <FaGithub className="w-6 h-4" />
+                          <a
+                            target="_blank"
+                            className="font-secondary mt-1  md:text-xl "
+                          >
+                            GitHub
+                          </a>
+                        </Button>
+                      </div>
+                    </div>
+                  </footer>
+                </article>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+    </motion.div>
+  </div>
+</section>;
