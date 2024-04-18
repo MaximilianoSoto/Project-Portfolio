5259e0f1fffe38b044e49691e0344c45e2d0f512
MaximilianoSoto <maximiliano.soto.guardia99@gmail.com>
2024-04-18 13:14:58 -0400

    update 2.8v

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -16,7 +16,7 @@ export const Skills = () => {
               initial="hidden"
               whileInView={"show"}
               viewport={{ once: false, amount: 0.3 }}
-              className="flex flex-col gap-y-5 border-accent border-t-4 border-b-4 text-left  md:py-[80px]  "
+              className="flex flex-col gap-y-5 border-accent border-t-4 border-b-4 text-left  md:py-[70px]  "
             >
               <h1 className="font-primary text-[20px]  text-center lg:text-left lg:text-[35px] font-semibold text-accent">
                 <TypeAnimation
